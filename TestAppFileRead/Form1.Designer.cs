c2624ad32e7d3ab2b8fc1cabbeb21b2264811a2b
Mark O Shea <osheamo@gmail.com>
2018-04-19 16:49:34 +0100

    Add project files.

---
+++ TestAppFileRead/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TestAppFileRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.getFileButton = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.getFileButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxFilePath, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.displayButton, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // getFileButton
+            // 
+            this.getFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.getFileButton.Location = new System.Drawing.Point(82, 41);
+            this.getFileButton.Name = "getFileButton";
+            this.getFileButton.Size = new System.Drawing.Size(75, 23);
+            this.getFileButton.TabIndex = 0;
+            this.getFileButton.Text = "Get File";
+            this.getFileButton.UseVisualStyleBackColor = true;
+            this.getFileButton.Click += new System.EventHandler(this.getFileButton_Click);
+            // 
+            // textBoxFilePath
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBoxFilePath, 2);
+            this.textBoxFilePath.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxFilePath.Location = new System.Drawing.Point(163, 44);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(474, 20);
+            this.textBoxFilePath.TabIndex = 1;
+            // 
+            // displayButton
+            // 
+            this.displayButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.displayButton.AutoSize = true;
+            this.displayButton.Location = new System.Drawing.Point(643, 41);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(89, 23);
+            this.displayButton.TabIndex = 3;
+            this.displayButton.Text = "Display Results";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 2);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(163, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 2);
+            this.dataGridView1.Size = new System.Drawing.Size(474, 241);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button getFileButton;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ TestAppFileRead/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestAppFileRead
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        //Get the file path selected
+        private void getFileButton_Click(object sender, EventArgs e)
+        {
+           
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = (openFileDialog1.FileName);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        //Only setup to Read in the data from a CSV file and display in the table
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Time");
+            dataTable.Columns.Add("Process Name");
+            dataTable.Columns.Add("PID");
+            dataTable.Columns.Add("Operation");
+            dataTable.Columns.Add("Path");
+            dataTable.Columns.Add("Result");
+            dataTable.Columns.Add("Detail1");
+            dataTable.Columns.Add("Detail2");
+            
+
+
+            string filePath = textBoxFilePath.Text;
+            StreamReader streamReader = new StreamReader(filePath);
+            string[] totalData = new string[File.ReadAllLines(filePath).Length];
+            totalData = streamReader.ReadLine().Split('"');
+            while (!streamReader.EndOfStream)
+            {
+                //Regex reg = new Regex("\"([^\"]*?)\"");
+                totalData = streamReader.ReadLine().Split('"');
+                //need to account for commas in offset and length values, not returning correct results
+
+
+                if (totalData[13].Contains("Offset: "))
+                {
+                    totalData[14] = totalData[13].Substring(8);
+                }
+
+
+                dataTable.Rows.Add( //totalData[0], 
+                                    totalData[1], 
+                                    //totalData[2], 
+                                    totalData[3], 
+                                    //totalData[4], 
+                                    totalData[5], 
+                                    //totalData[6],
+                                    totalData[7],
+                                    //totalData[8],
+                                    totalData[9],
+                                    //totalData[10],
+                                    totalData[11],
+                                    //totalData[12],
+                                    totalData[13],
+                                    totalData[14]
+                                    );
+            }
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
