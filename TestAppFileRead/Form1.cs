1c19287f4301d6e450aaddcc92812258140d4c4f
Mark O Shea <osheamo@gmail.com>
2018-04-23 10:23:58 +0100

    barchart

--- TestAppFileRead/Form1.cs
+++ TestAppFileRead/Form1.cs
@@ -53,7 +53,7 @@ namespace TestAppFileRead
         //Only setup to Read in the data from a CSV file and display in the table
         private void displayButton_Click(object sender, EventArgs e)
         {
-            string PID = "";
+   
             List<string> PIDlist = new List<string>();
             DataTable dataTable = new DataTable();
             dataTable.Columns.Add("Time");
@@ -93,18 +93,58 @@ namespace TestAppFileRead
             dataGridView1.DataSource = null;
             dataGridView1.DataSource = dataTable;
 
-            //    TO DO
+            var series = new Series("Process List");
+
+
             //find the rows with the matching PID's and then add the length for
             //each of these rows together and select the top ten values 
             //to populate the bar chart
+            //dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            Dictionary<string, List<int>> data = new Dictionary<string, List<int>>();
+            try
+            {
+                int length = 0;
+                int totalLength = 0;
+                string pid = "";
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    pid = row.Cells[5].ToString();
+                    length = Convert.ToInt32(row.Cells[14]);
+                    
+                    if (data.ContainsKey(pid))
+                        data[pid].Add(length);
+                    else data.Add(pid, new List<int>(new int[] { length }));
+                }
+               
+                foreach (string pidKey in data.Keys)
+                {                    
+                        totalLength += length;
+                        pid = pidKey;
+
+                }
+                
+                //need to sort the pid's by largest lengths and display the top ten.
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            series.Points.DataBindXY(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new[] { 100, 200, 90, 150, 20, 330, 500, 120, 50, 210 });
+            BarChart1.Series.Add(series);
         }
+
+    
 
 
 
         private void barChartButton_Click(object sender, EventArgs e)
         {
-            
 
+            var series = new Series("Process List");
+
+            // First parameter is X-Axis and Second is Collection of Y- Axis
+            series.Points.DataBindXY(new[] {1,2,3,4,5,6,7,8,9,10 }, new[] { 100, 200, 90, 150,20,330,500,120,50,210 });
+            BarChart1.Series.Add(series);
 
         }
        
